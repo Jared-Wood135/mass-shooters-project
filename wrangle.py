dbc4a4a953f215ab63b85b0a252d0714c5546822
Jared Wood <jared.wood135@gmail.com>
2023-04-24 11:06:51 -0500

    Template created for later use

---
+++ wrangle.py
@@ -0,0 +1,54 @@
+# =======================================================================================================
+# Table of Contents START
+# =======================================================================================================
+
+'''
+1. Orientation
+2. Imports
+3. wrangle_shooters
+'''
+
+# =======================================================================================================
+# Table of Contents END
+# Table of Contents TO Orientation
+# Orientation START
+# =======================================================================================================
+
+'''
+The purpose of this file is to wrangle (Acquire AND Prepare) the mass shooters dataframe
+for consistent and ease of use when exploring and modeling...
+'''
+
+# =======================================================================================================
+# Orientation END
+# Orientation TO Imports
+# Imports START
+# =======================================================================================================
+
+import numpy as np
+import pandas as pd
+import acquire
+import prepare
+
+# =======================================================================================================
+# Imports END
+# Imports TO wrangle_shooters
+# wrangle_shooters START
+# =======================================================================================================
+
+def wrangle_shooters():
+    '''
+    Acquires and prepares the mass_shooters dataframe for exploration...
+
+    INPUT:
+    NONE
+
+    OUTPUT:
+    store.csv = ONLY IF IT IS NONEXISTANT
+    newstore = pandas dataframe
+    '''
+    print('Template')
+
+# =======================================================================================================
+# wrangle_shooters END
+# =======================================================================================================

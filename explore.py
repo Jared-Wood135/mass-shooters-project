bc6a871634ebd250ef598efc9a32f22ddea6ae86
Jared Wood <jared.wood135@gmail.com>
2023-04-29 18:30:05 -0500

    .

--- explore.py
+++ explore.py
@@ -52,15 +52,15 @@ def binary_list():
     strong_ones_high_ratio_col = List of column names that have a 70%+ ratio for one, highvolatility values
     strong_ones_low_ratio_col = List of column names that have a 70%+ ratio for one, lowvolatility values
     '''
-    mass_shooters = w.prepare_mass_shooters()
+    train, validate, test = w.wrangle_mass_shooters()
     binary_cols = []
-    for col in mass_shooters.columns:
-        if mass_shooters[col].nunique() == 2:
+    for col in train.columns:
+        if train[col].nunique() == 2:
             binary_cols.append(col)
     binary_cols_passed_stats = []
     alpha = 0.05
     for col in binary_cols:
-        contingency_table = pd.crosstab(mass_shooters.shooter_volatility, mass_shooters[col])
+        contingency_table = pd.crosstab(train.shooter_volatility, train[col])
         stat, p_value, dof, expected = stats.chi2_contingency(contingency_table)
         if p_value < alpha:
             binary_cols_passed_stats.append(col)
@@ -69,12 +69,12 @@ def binary_list():
     strong_ones_high_ratio_col = []
     strong_ones_low_ratio_col = []
     for col in binary_cols_passed_stats:
-        total_zeros = (mass_shooters[col] == 0).sum()
-        total_ones = (mass_shooters[col] == 1).sum()
-        total_zeros_high = ((mass_shooters[col] == 0) & (mass_shooters.shooter_volatility == 'High Volatility')).sum()
-        total_zeros_low = ((mass_shooters[col] == 0) & (mass_shooters.shooter_volatility == 'Low Volatility')).sum()
-        total_ones_high = ((mass_shooters[col] == 1) & (mass_shooters.shooter_volatility == 'High Volatility')).sum()
-        total_ones_low = ((mass_shooters[col] == 1) & (mass_shooters.shooter_volatility == 'Low Volatility')).sum()
+        total_zeros = (train[col] == 0).sum()
+        total_ones = (train[col] == 1).sum()
+        total_zeros_high = ((train[col] == 0) & (train.shooter_volatility == 'High Volatility')).sum()
+        total_zeros_low = ((train[col] == 0) & (train.shooter_volatility == 'Low Volatility')).sum()
+        total_ones_high = ((train[col] == 1) & (train.shooter_volatility == 'High Volatility')).sum()
+        total_ones_low = ((train[col] == 1) & (train.shooter_volatility == 'Low Volatility')).sum()
         ratio_zeros_high = total_zeros_high / total_zeros
         ratio_zeros_low = total_zeros_low / total_zeros
         ratio_ones_high = total_ones_high / total_ones
@@ -105,16 +105,16 @@ def agg_list():
     OUTPUT:
     agg_cols_passed_stats = List of column names that passed exploratory filters for significance
     '''
-    mass_shooters = w.prepare_mass_shooters()
-    agg_cols = [col for col in mass_shooters.columns if col.startswith('agg')]
+    train, validate, test = w.wrangle_mass_shooters()
+    agg_cols = [col for col in train.columns if col.startswith('agg')]
     agg_cols_passed_stats = []
     alpha = 0.05
     for col in agg_cols:
-        target_binary = np.where(mass_shooters.shooter_volatility == 'High Volatility', 1, 0)
-        target_mean = sum(target_binary) / mass_shooters.shape[0]
-        non_target_mean = mass_shooters[col].mean()
-        stat, p = stats.ttest_ind_from_stats(mean1=target_mean, std1=1, nobs1=mass_shooters.shape[0],
-                                mean2=non_target_mean, std2=1, nobs2=mass_shooters.shape[0],
+        target_binary = np.where(train.shooter_volatility == 'High Volatility', 1, 0)
+        target_mean = sum(target_binary) / train.shape[0]
+        non_target_mean = train[col].mean()
+        stat, p = stats.ttest_ind_from_stats(mean1=target_mean, std1=1, nobs1=train.shape[0],
+                                mean2=non_target_mean, std2=1, nobs2=train.shape[0],
                                 equal_var=False)
         if p < alpha:
             agg_cols_passed_stats.append(col)

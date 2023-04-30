e4093a9f1cf22a01592add54483602cbcaf665d3
Jared Wood <jared.wood135@gmail.com>
2023-04-30 17:19:00 -0500

    .

--- final.py
+++ final.py
@@ -41,10 +41,11 @@ of the final_report.ipynb
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sns
 from scipy import stats
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn.ensemble import RandomForestClassifier as RFC
 from sklearn.metrics import classification_report, accuracy_score, recall_score
 import wrangle as w
 import explore as e
@@ -55,7 +56,26 @@ import explore as e
 # visual1 START
 # =======================================================================================================
 
+def visual1():
+    '''
+    Gets the 1st visual for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    Displays 1st visual
+    '''
+    mpl.style.use('bmh')
+    full = w.prepare_mass_shooters()
+    year_counts = full.year.value_counts().sort_index()
+    plt.plot(year_counts.index, year_counts.values, label='Mass Shooter Incidents')
+    sns.regplot(data=year_counts, x=year_counts.index, y=year_counts.values, scatter=False, ci=0, label='Trend Line')
+    plt.title('Mass Shooter Incidents By Year')
+    plt.xlabel('Year')
+    plt.ylabel('Count of Mass Shootings')
+    plt.legend()
+    plt.show()
 
 # =======================================================================================================
 # visual1 END
@@ -63,7 +83,36 @@ import explore as e
 # visual2 START
 # =======================================================================================================
 
+def visual2():
+    '''
+    Gets the 2nd visual for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    Displays 2nd visual
+    '''
+    mpl.style.use('bmh')
+    full = w.prepare_mass_shooters()
+    removed_outlier = full[full.agg_casualties != 927]
+    casualties_wo_outlier = removed_outlier.groupby('year')['agg_casualties'].sum()
+    casualties_w_outlier = full.groupby('year')['agg_casualties'].sum()
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+    axs[0].plot(casualties_wo_outlier.index, casualties_wo_outlier.values, label='Annual Casualties')
+    axs[0].set_title('Casualties By Year (Without Outlier)')
+    axs[0].set_xlabel('Year')
+    axs[0].set_ylabel('Total Casualties')
+    sns.regplot(data=casualties_wo_outlier, x=casualties_wo_outlier.index, y=casualties_wo_outlier.values, scatter=False, ci=0, label='Trend Line', ax=axs[0])
+    axs[0].legend()
+    axs[1].plot(casualties_w_outlier.index, casualties_w_outlier.values, label='Annual Casualties')
+    axs[1].set_title('Casualties By Year (With Outlier)')
+    axs[1].set_xlabel('Year')
+    axs[1].set_ylabel('Total Casualties')
+    sns.regplot(data=casualties_w_outlier, x=casualties_w_outlier.index, y=casualties_w_outlier.values, scatter=False, ci=0, label='Trend Line', ax=axs[1])
+    axs[1].legend()
+    plt.subplots_adjust(wspace=0.3)
+    plt.show()
 
 # =======================================================================================================
 # visual2 END
@@ -71,7 +120,22 @@ import explore as e
 # visual3 START
 # =======================================================================================================
 
+def visual3():
+    '''
+    Gets the 3rd visual for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    Displays 3rd visual
+    '''
+    mpl.style.use('bmh')
+    full = w.prepare_mass_shooters()
+    sns.histplot(data=full, x='shooter_volatility')
+    plt.title('Distribution of Shooter Volatility')
+    plt.gca().set_xticklabels(['High Volatility (Casualties > 10)', 'Low Volatility (Casualties <= 10)'])
+    plt.show()
 
 # =======================================================================================================
 # visual3 END
@@ -79,7 +143,25 @@ import explore as e
 # visual4 START
 # =======================================================================================================
 
+def visual4():
+    '''
+    Gets the 4th visual for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    Displays 4th visual
+    '''
+    mpl.style.use('bmh')
+    full = w.prepare_mass_shooters()
+    removed_outlier = full[full.agg_casualties != 927]
+    removed_outlier['agg_crime'] = removed_outlier['agg_crime'] * 32
+    sns.regplot(data=removed_outlier, x='agg_crime', y='agg_casualties', ci=0, line_kws={'color' : 'red'})
+    plt.title('Shooter Crime History vs. Casualties')
+    plt.xlabel('Number of Significant Crimes Convicted Out of 32 Types')
+    plt.ylabel('Casualties')
+    plt.show()
 
 # =======================================================================================================
 # visual4 END
@@ -87,7 +169,25 @@ import explore as e
 # visual5 START
 # =======================================================================================================
 
+def visual5():
+    '''
+    Gets the 5th visual for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    Displays 5th visual
+    '''
+    mpl.style.use('bmh')
+    full = w.prepare_mass_shooters()
+    removed_outlier = full[full.agg_casualties != 927]
+    removed_outlier['agg_trauma'] = removed_outlier['agg_trauma'] * 20
+    sns.regplot(data=removed_outlier, x='agg_trauma', y='agg_casualties', ci=0, line_kws={'color' : 'red'})
+    plt.title('Shooter Trauma History vs. Casualties')
+    plt.xlabel('Number of Significant Traumatic Events Out of 20 Types')
+    plt.ylabel('Casualties')
+    plt.show()
 
 # =======================================================================================================
 # visual5 END
@@ -95,7 +195,25 @@ import explore as e
 # visual6 START
 # =======================================================================================================
 
+def visual6():
+    '''
+    Gets the 6th visual for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    Displays 6th visual
+    '''
+    mpl.style.use('bmh')
+    full = w.prepare_mass_shooters()
+    removed_outlier = full[full.agg_casualties != 927]
+    removed_outlier['agg_grand_total'] = removed_outlier['agg_grand_total'] * 109
+    sns.regplot(data=removed_outlier, x='agg_grand_total', y='agg_casualties', ci=0, line_kws={'color' : 'red'})
+    plt.title('Shooter Abnormalities vs. Casualties')
+    plt.xlabel('Number of Abnormalities of 109 Types')
+    plt.ylabel('Casualties')
+    plt.show()
 
 # =======================================================================================================
 # visual6 END
@@ -103,7 +221,33 @@ import explore as e
 # stat1 START
 # =======================================================================================================
 
+def stat1():
+    '''
+    Gets the 1st statistic for the 4th visual for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    Displays 1st statistic for the 4th visual
+    '''
+    full = w.prepare_mass_shooters()
+    removed_outlier = full[full.agg_casualties != 927]
+    removed_outlier['agg_crime'] = removed_outlier['agg_crime'] * 32
+    target_binary = np.where(removed_outlier.shooter_volatility == 'High Volatility', 1, 0)
+    target_mean = sum(target_binary) / removed_outlier.shape[0]
+    non_target_mean = removed_outlier.agg_crime.mean()
+    stat, p = stats.ttest_ind_from_stats(mean1=target_mean, std1=1, nobs1=removed_outlier.shape[0],
+                            mean2=non_target_mean, std2=1, nobs2=removed_outlier.shape[0],
+                            equal_var=False)
+    if p < 0.05:
+        print(f'\033[32m========== REJECT NULL HYPOTHESIS! ==========\033[0m')
+        print(f'\033[35mStatistic:\033[0m {stat:.4f}')
+        print(f'\033[35mP-Value:\033[0m {p:.4f}')
+    else:
+        print(f'\033[31m========== ACCEPT NULL HYPOTHESIS! ==========\033[0m')
+        print(f'\033[35mStatistic:\033[0m {stat:.4f}')
+        print(f'\033[35mP-Value:\033[0m {p:.4f}')
 
 # =======================================================================================================
 # stat1 END
@@ -111,7 +255,33 @@ import explore as e
 # stat2 START
 # =======================================================================================================
 
+def stat2():
+    '''
+    Gets the 2nd statistic for the 5th visual for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    Displays 2nd statistic for the 5th visual
+    '''
+    full = w.prepare_mass_shooters()
+    removed_outlier = full[full.agg_casualties != 927]
+    removed_outlier['agg_trauma'] = removed_outlier['agg_trauma'] * 20
+    target_binary = np.where(removed_outlier.shooter_volatility == 'High Volatility', 1, 0)
+    target_mean = sum(target_binary) / removed_outlier.shape[0]
+    non_target_mean = removed_outlier.agg_trauma.mean()
+    stat, p = stats.ttest_ind_from_stats(mean1=target_mean, std1=1, nobs1=removed_outlier.shape[0],
+                            mean2=non_target_mean, std2=1, nobs2=removed_outlier.shape[0],
+                            equal_var=False)
+    if p < 0.05:
+        print(f'\033[32m========== REJECT NULL HYPOTHESIS! ==========\033[0m')
+        print(f'\033[35mStatistic:\033[0m {stat:.4f}')
+        print(f'\033[35mP-Value:\033[0m {p:.4f}')
+    else:
+        print(f'\033[31m========== ACCEPT NULL HYPOTHESIS! ==========\033[0m')
+        print(f'\033[35mStatistic:\033[0m {stat:.4f}')
+        print(f'\033[35mP-Value:\033[0m {p:.4f}')
 
 # =======================================================================================================
 # stat2 END
@@ -119,7 +289,33 @@ import explore as e
 # stat3 START
 # =======================================================================================================
 
+def stat3():
+    '''
+    Gets the 3rd statistic for the 6th visual for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    Displays 3rd statistic for the 6th visual
+    '''
+    full = w.prepare_mass_shooters()
+    removed_outlier = full[full.agg_casualties != 927]
+    removed_outlier['agg_grand_total'] = removed_outlier['agg_grand_total'] * 109
+    target_binary = np.where(removed_outlier.shooter_volatility == 'High Volatility', 1, 0)
+    target_mean = sum(target_binary) / removed_outlier.shape[0]
+    non_target_mean = removed_outlier.agg_grand_total.mean()
+    stat, p = stats.ttest_ind_from_stats(mean1=target_mean, std1=1, nobs1=removed_outlier.shape[0],
+                            mean2=non_target_mean, std2=1, nobs2=removed_outlier.shape[0],
+                            equal_var=False)
+    if p < 0.05:
+        print(f'\033[32m========== REJECT NULL HYPOTHESIS! ==========\033[0m')
+        print(f'\033[35mStatistic:\033[0m {stat:.4f}')
+        print(f'\033[35mP-Value:\033[0m {p:.4f}')
+    else:
+        print(f'\033[31m========== ACCEPT NULL HYPOTHESIS! ==========\033[0m')
+        print(f'\033[35mStatistic:\033[0m {stat:.4f}')
+        print(f'\033[35mP-Value:\033[0m {p:.4f}')
 
 # =======================================================================================================
 # stat3 END
@@ -127,7 +323,110 @@ import explore as e
 # models START
 # =======================================================================================================
 
+def models():
+    '''
+    Returns a pandas dataframe with a sample of models created during the modeling process
+    for the 'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    models = pandas dataframe with 20 models for demonstration
+    '''
+    train, validate, test = w.wrangle_mass_shooters()
+    models_dict = {
+    'model_name' : [],
+    'model_type' : [],
+    'model_descriptor' : [],
+    'accuracy_train' : [],
+    'accuracy_val' : [],
+    'accuracy_diff' : [],
+    'recall_variable' : [],
+    'recall_train' : [],
+    'recall_val' : [],
+    'recall_diff' : []
+    }
+    baseline_train = round((train.shooter_volatility == train.shooter_volatility.mode()[0]).sum() / train.shape[0], 3)
+    baseline_val = round((validate.shooter_volatility == validate.shooter_volatility.mode()[0]).sum() / validate.shape[0], 3)
+    models_dict['model_name'].append('Baseline')
+    models_dict['model_type'].append('Baseline')
+    models_dict['model_descriptor'].append('Mode = Low Volatility')
+    models_dict['accuracy_train'].append(baseline_train)
+    models_dict['accuracy_val'].append(baseline_val)
+    models_dict['accuracy_diff'].append(baseline_val - baseline_train)
+    models_dict['recall_variable'].append('High Volatility')
+    models_dict['recall_train'].append(0.000)
+    models_dict['recall_val'].append(0.000)
+    models_dict['recall_diff'].append(0.000)
+    zero_high, zero_low, one_high, one_low = e.binary_list()
+    zero_all = zero_high + zero_low
+    one_all = one_high + one_low
+    high_only = zero_high + one_high
+    low_only = zero_low + one_low
+    all_binary = zero_all + one_all
+    agg_list = e.agg_list()
+    agg_list = [name for name in agg_list if name != 'agg_casualties']
+    dictionary_list = {
+    'zero_high' : zero_high,
+    'zero_low' : zero_low,
+    'one_high' : one_high,
+    'one_low' : one_low,
+    'zero_all' : zero_all,
+    'one_all' : one_all,
+    'high_only' : high_only,
+    'low_only' : low_only,
+    'all_binary' : all_binary,
+    'agg_list' : agg_list
+    }
+    n = 0
+    for list in dictionary_list:
+        dtc = DTC(max_depth=3, random_state=1349, ccp_alpha=0.02)
+        dtc.fit(train[dictionary_list[list]], train.shooter_volatility)
+        train_preds = dtc.predict(train[dictionary_list[list]])
+        val_preds = dtc.predict(validate[dictionary_list[list]])
+        train_accuracy = round(accuracy_score(train.shooter_volatility, train_preds), 3)
+        val_accuracy = round(accuracy_score(validate.shooter_volatility, val_preds), 3)
+        accuracy_diff = val_accuracy - train_accuracy
+        train_recall = round(recall_score(train.shooter_volatility, train_preds, pos_label='High Volatility'), 3)
+        val_recall = round(recall_score(validate.shooter_volatility, val_preds, pos_label='High Volatility'), 3)
+        recall_diff = val_recall - train_recall
+        n += 1
+        models_dict['model_name'].append(f'DTC{n}')
+        models_dict['model_type'].append('Decision Tree Classifier')
+        models_dict['model_descriptor'].append(list)
+        models_dict['accuracy_train'].append(train_accuracy)
+        models_dict['accuracy_val'].append(val_accuracy)
+        models_dict['accuracy_diff'].append(accuracy_diff)
+        models_dict['recall_variable'].append('High Volatility')
+        models_dict['recall_train'].append(train_recall)
+        models_dict['recall_val'].append(val_recall)
+        models_dict['recall_diff'].append(recall_diff)
+    n = 0
+    for list in dictionary_list:
+        rfc = RFC(max_depth=3, random_state=1349, ccp_alpha=0.02)
+        rfc.fit(train[dictionary_list[list]], train.shooter_volatility)
+        train_preds = rfc.predict(train[dictionary_list[list]])
+        val_preds = rfc.predict(validate[dictionary_list[list]])
+        train_accuracy = round(accuracy_score(train.shooter_volatility, train_preds), 3)
+        val_accuracy = round(accuracy_score(validate.shooter_volatility, val_preds), 3)
+        accuracy_diff = val_accuracy - train_accuracy
+        train_recall = round(recall_score(train.shooter_volatility, train_preds, pos_label='High Volatility'), 3)
+        val_recall = round(recall_score(validate.shooter_volatility, val_preds, pos_label='High Volatility'), 3)
+        recall_diff = val_recall - train_recall
+        n += 1
+        models_dict['model_name'].append(f'RFC{n}')
+        models_dict['model_type'].append('Random Forest Classifier')
+        models_dict['model_descriptor'].append(list)
+        models_dict['accuracy_train'].append(train_accuracy)
+        models_dict['accuracy_val'].append(val_accuracy)
+        models_dict['accuracy_diff'].append(accuracy_diff)
+        models_dict['recall_variable'].append('High Volatility')
+        models_dict['recall_train'].append(train_recall)
+        models_dict['recall_val'].append(val_recall)
+        models_dict['recall_diff'].append(recall_diff)
+        models = pd.DataFrame(models_dict)
+        return models
 
 # =======================================================================================================
 # models END
@@ -135,7 +434,27 @@ import explore as e
 # models_best START
 # =======================================================================================================
 
+def models_best():
+    '''
+    Returns a pandas dataframe with the best model created during the modeling process for the 
+    'final_report.ipynb'...
 
+    INPUT:
+    NONE
+
+    OUTPUT:
+    top_model = pandas dataframe with top model for demonstration
+    '''
+    best_models_dict = {
+    'model_name' : ['Baseline', 'DTC6'],
+    'model_type' : ['Baseline', 'Decision Tree Classifier'],
+    'model_descriptor' : ['mode == "Low Volatility"', 'one_all'],
+    'accuracy' : [0.632, 0.632],
+    'recall_pos_label' : ['High Volatility', 'High Volatility'],
+    'recall' : [0.000, 0.429]
+    }
+    top_model = pd.DataFrame(best_models_dict)
+    return top_model
 
 # =======================================================================================================
 # models_best END
